ea1306281dab4404511860b5c79257223ccb8bbf
Rishi1314 <rishiraj13052003@gmail.com>
2024-11-30 19:39:12 -0500

    2 Grid items.

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -4,15 +4,17 @@ import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
 const Grid = () => {
     return (
       <div className="bg-[#010204] pb-5">
-            <BentoGrid className="max-w-4xl   mx-auto">
+            <BentoGrid className="w-[70vw]">
       {items.map((item, i) => (
         <BentoGridItem
           key={i}
+          id={item.id}
           title={item.title}
           description={item.description}
-          header={item.header}
-        //   icon={item.icon}
-          className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+          // header={item.header}
+          //   icon={item.icon}
+          className={item.className}
+          titleClassName={item.titleClassName}
         />
       ))}
     </BentoGrid>
@@ -21,52 +23,71 @@ const Grid = () => {
   )
 }
 
-const Skeleton = () => (
-    <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
-  );
+// const Skeleton = () => (
+//     <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+//   );
   const items = [
     {
-      title: "The Dawn of Innovation",
-      description: "Explore the birth of groundbreaking ideas and inventions.",
-      header: <Skeleton />,
-    //   icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-      title: "The Digital Revolution",
-      description: "Dive into the transformative power of technology.",
-      header: <Skeleton />,
-    //   icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-      title: "The Art of Design",
-      description: "Discover the beauty of thoughtful and functional design.",
-      header: <Skeleton />,
-    //   icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-      title: "The Power of Communication",
-      description:
-        "Understand the impact of effective communication in our lives.",
-      header: <Skeleton />,
-    //   icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-      title: "The Pursuit of Knowledge",
-      description: "Join the quest for understanding and enlightenment.",
-      header: <Skeleton />,
-    //   icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-      title: "The Joy of Creation",
-      description: "Experience the thrill of bringing ideas to life.",
-      header: <Skeleton />,
-    //   icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-      title: "The Spirit of Adventure",
-      description: "Embark on exciting journeys and thrilling discoveries.",
-      header: <Skeleton />,
-    //   icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
-    },
+    id: 1,
+    title: "I’m Rishi Raj Bobbarapalli, a Computer Science graduate student passionate about AI, web development, and solving real-world problems through technology. With experience in building innovative software and leading workshops, I enjoy creating solutions that inspire and empower. Outside of tech, I love basketball and collaborative challenges. ",
+    description: "Who am I?",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-start  text-2xl text-[#fff]",
+    // img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    // img: "/grid.svg",
+    // spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    // img: "/b5.svg",
+    // spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Let's Talk.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center text-xl font-bold text-white md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+    
   ];
 export default Grid
